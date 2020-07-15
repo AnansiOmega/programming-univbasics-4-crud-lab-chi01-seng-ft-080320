c433d19caf6fcd31b0dbe2248f0d96ae78783c88
Ignas Butautas <ignasbutautas7@gmail.com>
2020-07-15 21:48:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -41,7 +41,7 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-item =  array[index_number]
+#item =  array[index_number]
 array[index_number](element)
 
 end
